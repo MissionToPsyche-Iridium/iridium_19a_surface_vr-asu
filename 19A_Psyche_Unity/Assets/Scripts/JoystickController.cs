1d732942df9fe22fd8cebd6021073fb4e5bc8c9d
ap4godfrey227 <67652648+ap4godfrey227@users.noreply.github.com>
2024-11-06 16:19:49 -0700

    Joystick Physics v1 implemented
    Joystick currently can be moved around if grabbed by the top and is limited to the joystick base.
    
    Current Bugs:
    - joystick spazzes out if grabbed and moved far away from normal reaches
    - "value" of the simulated input from the joystick needs to be corrected

--- 19A_Psyche_Unity/Assets/Scripts/JoystickController.cs
+++ 19A_Psyche_Unity/Assets/Scripts/JoystickController.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class JoystickController : MonoBehaviour
 {
     
     public Transform topOfJoystick;
+    public Transform leftHand;
+    public Transform rightHand;
 
     [SerializeField] private float forwardTiltAxis = 0;
     [SerializeField] private float sideTiltAxis = 0;
+
+    private Vector3 anchor;
+    private Rigidbody topRb;
+
+    private bool leftHandGrabbed = false;
+    private bool rightHandGrabbed = false;
     
+
+    void Start()
+    {
+        topRb = topOfJoystick.gameObject.GetComponent<Rigidbody>();
+        anchor = topOfJoystick.GetComponent<CharacterJoint>().anchor;
+    }
 
     void Update()
     {
-        forwardTiltAxis = topOfJoystick.rotation.eulerAngles.x;
+        forwardTiltAxis = Vector3.Angle(anchor, new Vector3(topOfJoystick.position.x, topOfJoystick.position.y, 0));
         if(forwardTiltAxis < 355 && forwardTiltAxis > 290)
         {
             forwardTiltAxis = Mathf.Abs(forwardTiltAxis -360);
@@ -24,7 +40,7 @@ public class JoystickController : MonoBehaviour
             Debug.Log("Forward: " + forwardTiltAxis);
         }
 
-        sideTiltAxis = topOfJoystick.rotation.eulerAngles.z;
+        sideTiltAxis = Vector3.Angle(anchor, new Vector3(0, topOfJoystick.position.y, topOfJoystick.position.z));
         if(sideTiltAxis < 355 && sideTiltAxis > 290)
         {
             sideTiltAxis = Mathf.Abs(sideTiltAxis -360);
@@ -34,5 +50,37 @@ public class JoystickController : MonoBehaviour
         {
             Debug.Log("Left: " + sideTiltAxis);
         }
+        if(leftHandGrabbed)
+        {
+            
+        }
+        if(rightHandGrabbed)
+        {
+            
+        }
+        Debug.Log("Forward: " + forwardTiltAxis);
+        Debug.Log("Side: " + sideTiltAxis);
+    }
+
+    public void OnHandGrab()
+    {
+        if(Vector3.Distance(topOfJoystick.position, leftHand.position) < Vector3.Distance(topOfJoystick.position, rightHand.position))
+        {
+            leftHandGrabbed = true;
+            rightHandGrabbed = false;
+        }
+        else
+        {
+            leftHandGrabbed = false;
+            rightHandGrabbed = true;
+        }
+        topRb.constraints = RigidbodyConstraints.None;
+    }
+
+    public void OnHandRelease()
+    {
+        leftHandGrabbed = false;
+        rightHandGrabbed = false;
+        topRb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 }
