4e1e03a6642669c4f0660e83bbe3954e0f1cf0b8
ap4godfrey227 <67652648+ap4godfrey227@users.noreply.github.com>
2024-11-06 23:01:06 -0700

    JMARS_Testing Updated with rover
    Added semi movable rover to JMARS Testing map. Does not follow generated topology yet

--- 19A_Psyche_Unity/Assets/Scripts/JoystickController.cs
+++ 19A_Psyche_Unity/Assets/Scripts/JoystickController.cs
@@ -8,72 +8,60 @@ public class JoystickController : MonoBehaviour
 {
     
     public Transform topOfJoystick;
-    public Transform anchor;
-    public Transform leftHand;
-    public Transform rightHand;
     public Transform forwardDir;
     public Transform backwardDir;
     public Transform leftDir;
     public Transform rightDir;
 
 
-    [SerializeField] private float forwardTiltAxis = 0;
-    [SerializeField] private float sideTiltAxis = 0;
+    public float forwardTiltAxis = 0;
+    public float sideTiltAxis = 0;
 
+    private Transform roverStart;
+    private GameObject rover;
+    private Rigidbody roverRb;
     
     private Rigidbody topRb;
-    private Quaternion startingRot;
 
-    private bool leftHandGrabbed = false;
-    private bool rightHandGrabbed = false;
+    [SerializeField] private float roverForceMult = 10f;
     
 
     void Start()
     {
         topRb = topOfJoystick.gameObject.GetComponent<Rigidbody>();
-        startingRot = anchor.localRotation;
+        rover = GameObject.FindGameObjectWithTag("Rover");
+        roverRb = rover.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
 
         
-        forwardTiltAxis = Vector3.Angle(topOfJoystick.up, anchor.position);
-
-        sideTiltAxis = Vector3.Angle(topOfJoystick.right, anchor.position);
+        forwardTiltAxis = (Vector3.Distance(backwardDir.position, topOfJoystick.position) - Vector3.Distance(forwardDir.position, topOfJoystick.position))/0.25f;
+        sideTiltAxis = (Vector3.Distance(leftDir.position, topOfJoystick.position) - Vector3.Distance(rightDir.position, topOfJoystick.position))/0.25f;
         
-        if(leftHandGrabbed)
+        Debug.Log("Forward: " + forwardTiltAxis);
+        Debug.Log("Side: " + sideTiltAxis);
+
+        if(forwardTiltAxis > 0.05 || forwardTiltAxis < -0.05)
         {
-            
+            roverRb.AddForce(rover.transform.forward*forwardTiltAxis*roverForceMult, ForceMode.Acceleration);
         }
-        if(rightHandGrabbed)
+        if(sideTiltAxis > 0.05 || sideTiltAxis < -0.05)
         {
-            
+            roverRb.AddForce(rover.transform.right*sideTiltAxis*roverForceMult, ForceMode.Acceleration);
         }
-        //Debug.Log("Forward: " + forwardTiltAxis);
-        //Debug.Log("Side: " + sideTiltAxis);
-        Debug.Log("Forward: " + Vector3.Distance(forwardDir.position, topOfJoystick.position));
     }
 
     public void OnHandGrab()
     {
-        if(Vector3.Distance(topOfJoystick.position, leftHand.position) < Vector3.Distance(topOfJoystick.position, rightHand.position))
-        {
-            leftHandGrabbed = true;
-            rightHandGrabbed = false;
-        }
-        else
-        {
-            leftHandGrabbed = false;
-            rightHandGrabbed = true;
-        }
         topRb.constraints = RigidbodyConstraints.None;
+        roverRb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
     public void OnHandRelease()
     {
-        leftHandGrabbed = false;
-        rightHandGrabbed = false;
         topRb.constraints = RigidbodyConstraints.FreezeRotation;
+        roverRb.velocity = Vector3.zero;
     }
 }
