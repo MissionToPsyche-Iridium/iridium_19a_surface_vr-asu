f80c15291aaaba0a62921cffbcc17f8f1f9181dd
ap4godfrey227 <67652648+ap4godfrey227@users.noreply.github.com>
2024-11-25 14:38:38 -0700

    Created Button Prefab and Rover Reset System

---
+++ 19A_Psyche_Unity/Assets/Scripts/RoverResetButton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverResetButton : MonoBehaviour
+{
+
+    bool isPressed;
+
+    Transform roverStart;
+    GameObject rover;
+    Rigidbody roverRb;
+
+    private Transform unpressed;
+    private Transform pressed;
+    private Transform button;
+
+
+
+    void Start()
+    {
+        isPressed = false;
+        pressed = transform.GetChild(1);
+        unpressed = transform.GetChild(2);
+        button = transform.GetChild(3);
+
+        roverStart = GameObject.FindGameObjectWithTag("RoverStart").transform;
+        rover = GameObject.FindGameObjectWithTag("Rover");
+        roverRb = rover.GetComponent<Rigidbody>();
+    }
+
+    public void OnButtonPress()
+    {
+        isPressed = true;
+        button.transform.position = pressed.position;
+        rover.transform.position = roverStart.position;
+        rover.transform.rotation = roverStart.rotation;
+        roverRb.velocity = Vector3.zero;
+        roverRb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void OnButtonRelease()
+    {
+        isPressed = false;
+        button.transform.position = unpressed.position;
+    }
+}
