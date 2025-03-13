7df2331b3fa3d282fe171c6a1ed25e3d1b116230
bustlilcap <bustlilcap@gmail.com>
2025-03-13 10:46:57 -0700

    new file:   19A_Psyche_Unity/Assets/Prefabs/ParfaitTerrainMenu.prefab 	new file:   19A_Psyche_Unity/Assets/Prefabs/ParfaitTerrainMenu.prefab.meta 	new file:   19A_Psyche_Unity/Assets/Prefabs/TerrainMenu.prefab 	new file:   19A_Psyche_Unity/Assets/Prefabs/TerrainMenu.prefab.meta 	modified:   19A_Psyche_Unity/Assets/Prefabs/XR Origin (XR Rig).prefab 	modified:   19A_Psyche_Unity/Assets/Scenes/JMARS_TESTING.unity 	modified:   19A_Psyche_Unity/Assets/Scenes/NewTerrainTesting.unity 	modified:   19A_Psyche_Unity/Assets/Scripts/TerrainMenuButton.cs

--- 19A_Psyche_Unity/Assets/Scripts/TerrainMenuButton.cs
+++ 19A_Psyche_Unity/Assets/Scripts/TerrainMenuButton.cs
@@ -1,17 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TerrainEngine;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TerrainMenuButton : MonoBehaviour
 {
+    public enum buttonType {PARFAIT, TERRAIN, SCENE};
+
+    [SerializeField] private buttonType type;
+    [SerializeField] private GameObject terrain;
+    [SerializeField] private String unityScene;
     private SceneMaterializer sceneMaterializer;
     private DataPackBehaviour scene;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        sceneMaterializer = GameObject.Find("Terrain").GetComponent<SceneMaterializer>();
-        scene = GetComponent<DataPackBehaviour>();
+        switch (type)
+        {
+            case buttonType.PARFAIT:
+                sceneMaterializer = GameObject.Find("Terrain").GetComponent<SceneMaterializer>();
+                scene = GetComponent<DataPackBehaviour>();
+                break;
+
+            
+        }
+        
     }
 
     // Update is called once per frame
@@ -24,6 +42,25 @@ public class TerrainMenuButton : MonoBehaviour
 
     public void LoadScene()
     {
-        sceneMaterializer.LoadTerrain(scene);
+        switch(type)
+        {
+            case buttonType.PARFAIT:
+                sceneMaterializer.LoadTerrain(scene);
+                break;
+
+            case buttonType.SCENE:
+                SceneManager.LoadScene(unityScene);
+                break;
+
+            case buttonType.TERRAIN:
+                GameObject[] terrains = GameObject.FindGameObjectsWithTag("Terrain");
+                foreach (GameObject t in terrains)
+                {
+                    t.SetActive(false);
+                }
+                terrain.SetActive(true);
+                break;
+        }
+        
     }
 }
